1924fe576ab9eb1a097e707df9c47006416b1b35
Milap Keshwala <mkeshwala06@gmail.com>
2025-08-15 18:31:32 +0100

    Switch's to PostgreSQL instead of SQLite

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'poker_db',
+        'USER': 'poker_user',
+        'PASSWORD': 'MKrandom1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
