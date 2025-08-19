690869318d0d59eb0100700412345cd8ed79cb61
Milap Keshwala <mkeshwala06@gmail.com>
2025-08-19 18:47:48 +0100

    Adds initial poker game UI(WIP)

---
+++ frontend/src/pages/PokerGame.jsx
@@ -0,0 +1,51 @@
+import "../styles/PokerGame.css";
+import { Button } from "@mui/material";
+import { useState } from "react";
+
+export default function PokerGame() {
+    const [gameState, setGameState] = useState({
+        stakes: { small: 1, big: 2 },
+        players: [
+            { id: 1, chips: 250000, active: true },
+            { id: 2, chips: 250, active: false },
+            { id: 3, chips: 250, active: false },
+            { id: 4, chips: 2500, active: false },
+            { id: 5, chips: 250, active: false },
+            { id: 6, chips: 250, active: false },
+        ],
+    });
+
+    const PlayerSeat = ({ position, chips, isActive }) => {
+        return (
+            <div
+                className={`seat seat-${position}`}
+            >   
+                <div className={`player-circle ${isActive ? "active" : ""}`}></div>
+                <div id="player-name">Player {position}</div>
+                <div id="player-chips">{chips}</div>
+            </div>
+        );
+    };
+
+    return (
+        <div id="game-root">
+            <div id="game-container">
+                <div id="game-leave-button">
+                    <Button variant="contained" color="secondary">
+                        Leave
+                    </Button>
+                </div>
+                <div id="game-poker-table">
+                    {gameState.players.map((player) => (
+                        <PlayerSeat
+                            key={player.id}
+                            position={player.id}
+                            chips={player.chips}
+                            isActive={player.active}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
