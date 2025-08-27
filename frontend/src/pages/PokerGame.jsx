1859a57aaff9166aa282b2fa0faed2ef28ed6e89
Milap Keshwala <mkeshwala06@gmail.com>
2025-08-27 12:40:10 +0100

    Dispays seats for each player in the table

--- frontend/src/pages/PokerGame.jsx
+++ frontend/src/pages/PokerGame.jsx
@@ -1,4 +1,5 @@
 import "../styles/PokerGame.css";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import { Button } from "@mui/material";
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
@@ -10,52 +11,68 @@ export default function PokerGame() {
     const navigate = useNavigate();
     const socketRef = useRef(null);
     const [gameState, setGameState] = useState({
-        stakes: { small: 1, big: 2 },
-        players: [
-            { id: 1, chips: 250000, active: true },
-            { id: 2, chips: 250, active: false },
-            { id: 3, chips: 250, active: false },
-            { id: 4, chips: 2500, active: false },
-            { id: 5, chips: 250, active: false },
-            { id: 6, chips: 250, active: false },
-        ],
+        stakes: { small: 0, big: 0 },
+        num_of_players: 0,
+        players: []
     });
 
     useEffect(() => {
-        const socket = new WebSocket('ws://localhost:8000/ws/poker/')
+        const access = localStorage.getItem(ACCESS_TOKEN);
+        const socket = new WebSocket(`${import.meta.env.VITE_WS_URL}/ws/poker/${access}/`);
+
+        socketRef.current = socket;
 
         socket.onopen = () => {
-            console.log('WebSocket connection established');
+            console.log("WebSocket connection established");
         };
 
         socket.onmessage = (event) => {
             const data = JSON.parse(event.data);
-            console.log('Received message:', data);
-        };
-         
-        socket.onclose = () => {
-            console.log('WebSocket connection closed');
+            if (data.event === 'game_joined') {
+                updateGameState(data);
+            }
+            
+            if (data.event === 'player_joined') {
+                updateGameState(data);
+            }
         };
 
-        socketRef.current = socket;
+        socket.onclose = () => {
+            console.log("WebSocket connection closed");
+        };
 
         return () => {
             if (socketRef.current) {
                 socketRef.current.close();
             }
-          };
-    }, [])
+        };
+    }, []);
 
-    const PlayerSeat = ({ position, chips, isActive }) => {
-        return (
-            <div className={`seat seat-${position}`}>
-                <div
-                    className={`player-circle ${isActive ? "active" : ""}`}
-                ></div>
-                <div id="player-name">Player {position}</div>
-                <div id="player-chips">{chips}</div>
-            </div>
-        );
+    const updateGameState = (gameData) => {
+        const transformedPlayers = [];
+        
+        for (let position = 1; position <= 6; position++) {
+            const seatData = gameData.data.seats[position];
+            
+            if (seatData) {
+                transformedPlayers.push({
+                    position: position,
+                    username: seatData.username,
+                    chips: seatData.chips,
+                    playerId: seatData.id,
+                    actualSeat: seatData.actual_seat,
+                    active: false
+                });
+            }
+        }
+        setGameState({
+            stakes: {
+                small: gameData.small_blind,
+                big: gameData.big_blind
+            },
+            num_of_players: gameData.num_of_players,
+            players: transformedPlayers
+        });
     };
 
     const handleLeaveGame = async (e) => {
@@ -67,7 +84,7 @@ export default function PokerGame() {
                 setAlertMessage(error.response.data.error);
             } else {
                 setAlertMessage(
-                    "Could not join a game. Please refresh or try again later!"
+                    "Could not leave a game. Please refresh or try again later!"
                 );
             }
             navigate("/");
@@ -91,17 +108,17 @@ export default function PokerGame() {
                     >
                         Leave
                     </Button>
-                </div>
+                </div>                
                 <div id="game-poker-table">
                     {gameState.players.map((player) => (
-                        <PlayerSeat
-                            key={player.id}
-                            position={player.id}
-                            chips={player.chips}
-                            isActive={player.active}
-                        />
+                        <div key={player.playerId} className={`seat seat-${player.position}`}>
+                            <div className={`player-circle ${player.active ? "active" : ""}`}></div>
+                            <div id="player-name">{player.username}</div>
+                            <div id="player-chips">${player.chips}</div>
+                        </div>
                     ))}
                 </div>
+                
                 <div id="game-buttons">
                     <Button className="action-btn fold-btn" variant="contained">
                         Fold
@@ -109,10 +126,7 @@ export default function PokerGame() {
                     <Button className="action-btn call-btn" variant="contained">
                         Call/Check
                     </Button>
-                    <Button
-                        className="action-btn raise-btn"
-                        variant="contained"
-                    >
+                    <Button className="action-btn raise-btn" variant="contained">
                         Bet/Raise
                     </Button>
                 </div>
