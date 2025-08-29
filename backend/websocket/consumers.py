a771772764fca3ea00f157cab2770043d6e99581
Milap Keshwala <mkeshwala06@gmail.com>
2025-08-29 21:34:13 +0100

    Feat: Adds player left event

--- backend/websocket/consumers.py
+++ backend/websocket/consumers.py
@@ -1,15 +1,15 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from .utils import *
+from .utils.game import *
 
 class PokerGameConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         self.token = self.scope['url_route']['kwargs']['token']
         self.user = await get_user(self.token)
-        game = await user_in_game(self.user)
-        if self.user and game:
-            self.room_group_name = f'poker_{game.id}'
+        self.game = await user_in_game(self.user)
+        if self.user and self.game:
+            self.room_group_name = f'poker_{self.game.id}'
             await self.accept()
 
             await self.channel_layer.group_add(self.room_group_name, self.channel_name)
@@ -21,7 +21,7 @@ class PokerGameConsumer(AsyncWebsocketConsumer):
             await self.close()
     
     async def disconnect(self, code):
-        pass
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
@@ -32,5 +32,13 @@ class PokerGameConsumer(AsyncWebsocketConsumer):
             await self.send(text_data=json.dumps({'event': 'game_joined', 'data': game_info}))
         else:
             await self.send(text_data=json.dumps({'event': 'player_joined', 'data': game_info}))
-    
+
+    async def player_left(self, event):
+        user_id = event.get('user_id')
+        if user_id == self.user.id:
+            leave_game(self.user)
+            await self.send(text_data=json.dumps({'event': 'game_leave'}))
+        else:
+            await self.send(text_data=json.dumps({'event': 'player_left', 'user_id': user_id}))
+
                 
