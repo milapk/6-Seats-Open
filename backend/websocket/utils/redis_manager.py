e6ae50edfa91efc3175d748c793441e883aa9693
Milap Keshwala <mkeshwala06@gmail.com>
2025-09-04 16:39:12 +0100

    Stores channel name in redis based on seat number the player has

--- backend/websocket/utils/redis_manager.py
+++ backend/websocket/utils/redis_manager.py
@@ -2,6 +2,7 @@ import json
 import redis.asyncio as redis
 from dotenv import load_dotenv
 import os
+from api.models import PlayerModel
 load_dotenv()
 
 REDIS_URL = os.environ.get('REDIS_URL')
@@ -22,5 +23,22 @@ async def get_game(game_id):
     Retrieve game state from Redis
     '''
     key = f'game:{game_id}:state'
+    r = await _get_redis()
+    return await r.get(key)
+
+async def set_player_channel(game_id, user, channel_name):
+    '''
+    Sets the channel name to the seat number of the player
+    '''
+    seat_num = (PlayerModel.objects.get(user=user)).seat_number
+    key = f'game:{game_id}:seat:{seat_num}'
+    r = await _get_redis()
+    return await r.set(key, channel_name)
+
+async def get_player_channel(game_id, seat_num):
+    '''
+    Retrieves the channel name
+    '''
+    key = f'game:{game_id}:seat:{seat_num}'
     r = await _get_redis()
     return await r.get(key)
