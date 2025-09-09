7a235799f1d08a0b4868bea43a91a293b97bed9a
Milap Keshwala <mkeshwala06@gmail.com>
2025-09-09 14:46:48 +0100

    Test: See how many cards there are once cards dealt

---
+++ backend/api/tests/card_count.py
@@ -0,0 +1,9 @@
+cards = 'Kc,Ks,9h,5h,2h,Ad,Ts,4c,Jd,6d,Ah,Jc,Js,7c,3h,Kd,Td,7s,8c,As,7h,2s,8h,Tc,3s,Qc,Th,9c,4h,4d,5d,3d,6h,8d,Qs,7d,Qh,6c,5c,8s,Jh,3c,5s,2c,Kh,Ac,Qd,6s'
+cards = cards.split(',')
+count = len(cards)
+print(count)
+#Output = 48
+
+''''
+Test if _deal_cards() works, it does since it deal 2 cards to the 2 players in the game
+'''
